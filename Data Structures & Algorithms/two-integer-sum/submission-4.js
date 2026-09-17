ce94ea28e15bbc77a2ed1362f767eb4bb74f7d43
tummistie <135588140+tummistie@users.noreply.github.com>
2026-09-17 18:27:20 -0400

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        for(let i = 0; i < nums.length - 1; i++){
+            for(let j = i + 1; j < nums.length; j++){ 
+                if(target === (nums[i] + nums[j])){
+                    return [i, j];
+                }
+            }
+        }
+
+        return [];
+    }
+}
